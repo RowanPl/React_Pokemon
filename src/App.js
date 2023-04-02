5c49d516bc90fcd89a5b40664f761f3516a3ce01
RowanPl <rowanplooij@hotmail.com>
2023-04-02 14:38:07 +0200

    homework finished, Plus extra

--- src/App.js
+++ src/App.js
@@ -1,16 +1,56 @@
-import React, {useEffect, useState} from 'react';
+//import React, {useEffect, useState} from 'react';
 import './App.css';
 import PokemonCard from "./PokemonCard/PokemonCard";
+import {useEffect, useState} from "react";
+import axios from "axios";
+import pokemonLogo from "./assets/Daco_5394286.png"
 
 function App() {
 const [endPoint , setEndPoint] = useState('https://pokeapi.co/api/v2/pokemon/')
+    const [pokemons, setPokemons] = useState('')
 
+
+    useEffect(() => {
+        async function fetchData() {
+            try {
+                const {data} = await axios.get(endPoint);
+                setPokemons(data);
+            } catch (e) {
+                console.error(e);
+            }
+        }
+        fetchData();
+    }, [endPoint]);
 
     return (
-    <div>
+    <div className='display'>
+        <img className="pokemonLogo" src={pokemonLogo} alt="Pokemon logo" onClick={() => setEndPoint('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')}/>
+        <section className="buttons">
+        <div className="pokeball-top-half">
+            <div className="pokeball-bottom-half">
+                <button
+                    className="pokeball-button"
+                    disabled={!pokemons.previous}
+                    onClick={() => setEndPoint(pokemons.previous)}>Previous</button>
+        </div>
+            </div>
+            <div className="pokeball-top-half">
+                <div className="pokeball-bottom-half">
+                    <button
+                        className="pokeball-button"
+                        disabled={!pokemons.next}
+                        onClick={() => setEndPoint(pokemons.next)}>Next</button>
 
-        <PokemonCard endpoint={endPoint}/>
+                </div>
+            </div>
+        </section>
 
+
+            <div className='Cards'>
+            {pokemons.results && pokemons.results.map((pokemon) => {
+                return <PokemonCard key={pokemon.name} endPoint={pokemon.url} />
+            })}
+            </div>
     </div>
   );
 }
--- src/PokemonCard/PokemonCard.js
+++ src/PokemonCard/PokemonCard.js
@@ -1,42 +1,54 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios";
+import "./PokemonCard.css";
 
-function PokemonCard(endPoint) {
+function PokemonCard({endPoint}) {
     const [cardData, setCardData] = useState("");
 
     useEffect(() => {
         async function fetchCard() {
             try {
-                const response = await axios.get({endPoint}.toString())
-                setCardData(response.data)
+                const {data} = await axios.get(endPoint)
+                setCardData(data)
             } catch (e) {
                 console.error(e);
             }
         }
 
         void fetchCard()
-    }, []);
+    }, [endPoint]);
 
 
     return (
         <>
-            {cardData !== "" && <div>
-                <p><strong>{cardData.name.toUpperCase()}</strong></p> <p>HP<strong>{cardData.stats[0].base_stat}</strong></p>
-                <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${cardData.id}.png`}
-                     alt={`"Normal ${cardData.name}"`}
-                     onMouseEnter={ e => (e.currentTarget.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/${cardData.id}.png` ) }
-                     onMouseOut={ e => (e.currentTarget.src = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${cardData.id}.png`)}
+            {cardData !== "" &&<section className="border">
+                <div className={`${cardData.types[0].type.name} outline`}>
+                    <div className="name-hp">
+                <p className="name"><strong>{cardData.name.toUpperCase()}</strong></p> <p className="hp">HP</p><p><strong>{cardData.stats[0].base_stat}</strong></p></div>
+                <img src={ cardData.sprites.front_default}
+                     alt={`${cardData.name}`}
+                     onMouseEnter={ e => (e.currentTarget.src = cardData.sprites.front_shiny ) }
+                     onMouseOut={ e => (e.currentTarget.src = cardData.sprites.front_default)}
                 />
+                    <div className='underline'></div>
+               <div className="textArea">
+                   <div className="colorfix">
                 <p>ID:{cardData.id} HT:{cardData.height}' WT:{cardData.weight} lbs</p>
                 <p>{cardData.moves.length} moves</p>
-                <div>
-                    <h3>Abilities:</h3>
-                    <p>{cardData.abilities[0].ability.name}</p>
-                    <p>{cardData.abilities[1].ability.name}</p>
-                    {(cardData.abilities[2]) !== undefined && <p>{cardData.abilities[2].ability.name}</p>}
-                </div>
-            </div>}
 
+                    <ul>
+                        {cardData.abilities.map((ability) => {
+                            return (
+                                <li key={`${ability.ability.name}-${cardData.name}`}>
+                                    {ability.ability.name}
+                                </li>
+                            )
+                        })}
+                    </ul>
+               </div>
+               </div>
+            </div>
+            </section>}
         </>
     );
 }
